62f277bdb2b64cc8f91e6ac9f0c6bfe1638cca3f
jhael07fmp <jhrodriguez@fmp.com.do>
2024-06-08 16:00:54 -0400

    everything is done

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Youtube-Clone",
   plugins: [react(), removeConsole()],
 });
